7a666a1658985f10c9e1b8cbb0e401b307dbdaf8
cjcnkolhe <cjcnkolhe@gmail.com>
2025-12-23 19:15:13 +0530

    first project commit

---
+++ StudentManagementSystem/src/main/java/edu/cjc/sms/app/controller/AdminController.java
@@ -0,0 +1,16 @@
+package edu.cjc.sms.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+	
+	
+	@RequestMapping("/")
+	public String preLogin() {
+		
+		return "login";
+	}
+
+}
